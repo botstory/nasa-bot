8e2f933a29d8cc7866d9ccc109639f99e8f5e745
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2017-05-14 12:48:05 +0200

    add comments about animation recycling

--- nasabot/geo/animation/__main__.py
+++ nasabot/geo/animation/__main__.py
@@ -6,6 +6,22 @@ import uuid
 
 
 async def main():
+    # TODO: recycle already exist animations (and downloaded files?)
+    # 1. hash parameters
+    # hash_of_settings = hash((
+    #     'https://gibs.earthdata.nasa.gov/wmts/{projection}/best/{layer}/default/{date}/{resolution}/{z}/{y}/{x}.jpg',
+    #     'MODIS_Terra_CorrectedReflectance_TrueColor',
+    #     'epsg3857',
+    #     2, 1, 2,
+    #     datetime.datetime.now() - datetime.timedelta(days=1, weeks=2),
+    #     datetime.datetime.now() - datetime.timedelta(days=1),
+    # ))
+    #
+    # 2. compare new parameters with existing hashes
+    # 3. on collision compare parameters
+    # 4. return already exist file
+    # 5. store popularity of files and remove files that is rare used.
+    hash_of_settings = uuid.uuid4()
     dir_path = os.getcwd()
     await pipeline(
         source=source.GIBSSource(
@@ -18,11 +34,11 @@ async def main():
             z=2,
         ),
         timeline=timeline.Interval(
-            datetime.date.now() - datetime.timedelta(days=1, weeks=2),
-            datetime.date.now() - datetime.timedelta(days=1),
+            datetime.datetime.now() - datetime.timedelta(days=1, weeks=2),
+            datetime.datetime.now() - datetime.timedelta(days=1),
         ),
         target=target.Gif(
-            os.path.join(dir_path, 'tmp', 'animation-{}.gif'.format(uuid.uuid4())),
+            os.path.join(dir_path, 'tmp', 'animation-{}.gif'.format(hash_of_settings)),
         )
     )
 
