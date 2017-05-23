377dd79b71bc9b76cfb0cbc2a08fc0cbf1f98570
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2017-05-24 00:39:44 +0200

    show map animation

--- nasabot/query/query_stories.py
+++ nasabot/query/query_stories.py
@@ -2,10 +2,11 @@ from botstory.middlewares import any, location, option, sticker, text
 from botstory.ast.story_context import get_message_attachment
 import emoji
 import datetime
-from datetime import date, timedelta
 import logging
 from nasabot.geo import animation, tiles
 import os
+from urllib.parse import urljoin
+import uuid
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +16,7 @@ satellite_image_epsg4326 = 'https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/M
 
 
 def day_before():
-    return date.today() - timedelta(days=1)
+    return datetime.datetime.now() - datetime.timedelta(days=1)
 
 
 def setup(story):
@@ -47,11 +48,22 @@ def setup(story):
             }, ],
         )
 
-    async def show_animation(ctx, target_data, lat, long, level):
-        tile = tiles.wgs84_tile_by_coors(lat, long, level)
-        await story.say('Here is the last 2 weeks:',
+    async def show_animation(ctx, target_date, lat, long, level):
+        tile = tiles.mercator_tile_by_coords(lat, long, level)
+        await story.say('Here is the last 2 weeks...',
                         user=ctx['user'])
-        gif_filename = os.path.join(dir_path, 'tmp', 'tmp-file-name.gif')
+        await story.start_typing(user=ctx['user'])
+
+        gif_filename = 'animation-{}.gif'.format(uuid.uuid4())
+        gif_full_filename = os.path.join(os.environ.get('GENERATED_STATIC_DIR'), gif_filename)
+        gif_url = urljoin(os.environ.get('HOST_URL'),
+                          os.path.join(os.environ.get('GENERATED_STATIC_PATH'), gif_filename))
+
+        logger.info('# tile')
+        logger.info(tile)
+        logger.info('# level')
+        logger.info(level)
+
         await animation.pipeline(
             source=animation.source.GIBSSource(
                 'https://gibs.earthdata.nasa.gov/wmts/{projection}/best/{layer}/default/{date}/{resolution}/{z}/{y}/{x}.jpg',
@@ -62,18 +74,35 @@ def setup(story):
                 **tile,
             ),
             timeline=animation.timeline.Interval(
-                datetime.datetime.now() - datetime.timedelta(days=1, weeks=2),
-                datetime.datetime.now() - datetime.timedelta(days=1),
+                target_date - datetime.timedelta(weeks=2),
+                target_date,
             ),
             target=animation.target.Gif(
-                gif_filename,
+                gif_full_filename,
             ),
         )
-        await story.send_image(
-            gif_filename,
-            user=ctx['user'],
-        )
-        os.remove(gif_filename)
+        await story.say(
+            emoji.emojize('Processed. Now we are going to upload it :package:.'),
+            user=ctx['user'])
+        await story.start_typing(user=ctx['user'])
+        await story.send_image(gif_url,
+                               user=ctx['user'])
+        await story.stop_typing(user=ctx['user'])
+
+        # show static image
+        #
+        # await story.send_image(
+        #     satellite_image_epsg3857.format(
+        #     # satellite_image_epsg4326.format(
+        #         **tile,
+        #         date=target_date.isoformat(),
+        #         z=level,
+        #     ),
+        #     user=ctx['user'],
+        # )
+        await story.say('What is next?',
+                        user=ctx['user'])
+        os.remove(gif_full_filename)
 
     @story.on(text.EqualCaseIgnore('earth'))
     def handle_random_location():
@@ -104,6 +133,7 @@ def setup(story):
     def handle_list_of_coords():
         @story.part()
         async def use_passed_coords_to_show_earth(ctx):
+            logger.info('# use_passed_coords_to_show_earth')
             raw_text = text.get_raw_text(ctx)
             values = raw_text.split(',')
             if len(values) < 2 or len(values) > 4:
@@ -116,7 +146,7 @@ def setup(story):
             else:
                 zoom = 6
 
-            await show_image(ctx, day_before(), lat, long, zoom)
+            await show_animation(ctx, day_before(), lat, long, zoom)
 
     @story.on(location.Any())
     def handle_location():
