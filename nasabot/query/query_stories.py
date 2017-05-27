8136fea7bc34dedc559e716ca33513be57604366
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2017-05-28 00:21:01 +0200

    could manual retry animation

--- nasabot/query/query_stories.py
+++ nasabot/query/query_stories.py
@@ -1,4 +1,4 @@
-import aiohttp
+from botstory.ast import story_context
 from botstory.ast.story_context import get_message_attachment
 from botstory.middlewares import any, location, option, sticker, text
 from botstory.integrations.commonhttp import errors as http_errors
@@ -19,6 +19,43 @@ satellite_image_epsg4326 = 'https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/M
 
 def day_before():
     return datetime.datetime.now() - datetime.timedelta(days=1)
+
+
+class ContextException(Exception):
+    pass
+
+
+class UserDialogContext:
+    """
+    store context of user dialog and get used context to reduce similar questions
+
+    """
+
+    def __init__(self, ctx):
+        self.ctx = ctx
+        self.user_data = story_context.get_user_data(ctx)
+
+    def get_last_location(self):
+        """
+        get last used coords
+        :return:
+        """
+        # TODO: raise exception if we don't have coors
+        try:
+            return self.user_data['coors'][-1]
+        except KeyError:
+            raise ContextException()
+
+    def store_location(self, lat, long, zoom=None, name=None):
+        if 'coors' not in self.user_data:
+            self.user_data['coors'] = []
+
+        self.user_data['coors'].append({
+            'lat': lat,
+            'long': long,
+            'zoom': zoom,
+            'name': name,
+        })
 
 
 def setup(story):
@@ -106,6 +143,21 @@ def setup(story):
                         user=ctx['user'])
         os.remove(gif_full_filename)
 
+    async def show_animation_or_ask_retry_on_fail(ctx, lat, long, zoom):
+        try:
+            await show_animation(ctx, day_before(), lat, long, zoom)
+        except http_errors.HttpRequestError as ex:
+            logger.warning('# got exception')
+            await story.ask(
+                emoji.emojize(':confused: Got error:\n\n{}\n\nPlease retry.'.format(ex.message),
+                              use_aliases=True),
+                quick_replies=[{
+                    'title': 'Retry {},{},{}'.format(lat, long, zoom),
+                    'payload': 'RETRY_SHOW_EARTH_{},{},{}'.format(lat, long, zoom),
+                }],
+                user=ctx['user']
+            )
+
     @story.on(text.EqualCaseIgnore('earth'))
     def handle_random_location():
         @story.part()
@@ -131,6 +183,24 @@ def setup(story):
         async def show_asia(ctx):
             await show_image(ctx, day_before(), 0, 170, 2)
 
+    @story.on([text.EqualCaseIgnore('retry'),
+               option.Match('RETRY_(.+)')])
+    def handle_retry():
+        @story.part()
+        async def use_store_coors_to_show_earth(ctx):
+            logger.info('# use_store_coors_to_show_earth')
+            dlg = UserDialogContext(ctx)
+            try:
+                location = dlg.get_last_location()
+                await show_animation_or_ask_retry_on_fail(
+                    ctx=ctx,
+                    lat=location['lat'],
+                    long=location['long'],
+                    zoom=location['zoom'],
+                )
+            except ContextException:
+                logger.warning('# we do not have needed user context')
+
     @story.on(text.Any())
     def handle_list_of_coords():
         @story.part()
@@ -148,19 +218,15 @@ def setup(story):
             else:
                 zoom = 6
 
-            try:
-                await show_animation(ctx, day_before(), lat, long, zoom)
-            except http_errors.HttpRequestError as ex:
-                logger.warning('# got exception')
-                await story.ask(
-                    emoji.emojize(':confused: Got error:\n\n{}\n\nPlease retry.'.format(ex.message),
-                                  use_aliases=True),
-                    quick_replies=[{
-                        'title': 'Retry {},{},{}'.format(lat, long, zoom),
-                        'payload': 'RETRY_SHOW_EARTH_{},{},{}'.format(lat, long, zoom),
-                    }],
-                    user=ctx['user']
-                )
+            dlg = UserDialogContext(ctx)
+            dlg.store_location(lat=lat, long=long, zoom=zoom)
+
+            await show_animation_or_ask_retry_on_fail(
+                ctx=ctx,
+                lat=lat,
+                long=long,
+                zoom=zoom,
+            )
 
     @story.on(location.Any())
     def handle_location():
